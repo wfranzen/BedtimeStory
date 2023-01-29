5db67a237cb1e19b508f3be7832cf8152f794781
Tarun <starkiller.1413@gmail.com>
2023-01-29 09:02:47 -0500

    GlobalVar

---
+++ bedtime/bedtimestory/bedtimestory/Util/GlobalVar.swift
@@ -0,0 +1,16 @@
+//
+//  GlobalVar.swift
+//  bedtimestory
+//
+//  Created by Tarun  on 1/29/23.
+//
+import Foundation
+import SwiftUI
+
+class GlobalState: ObservableObject {
+    
+    @Published var PageNum: Int = 0
+
+}
+
+
